08ff954f9651da544dca93f6c7c7b4b3fd10aa8e
koichi <111332153+koichi2426@users.noreply.github.com>
2023-08-13 21:58:28 +0900

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
+import './App.CSS';
+import React ,{ useState } from 'react';
+import App from './App.jsx';
 
-function App() {
+export default function App() {
+  // Home画面に遷移する関数
+  function goHome() {
+    window.location.href = './'
+  }
+
+  //目標達成プロセス画面に遷移する関数
+  function goPage1() {
+    window.location.href = './Page1'
+  }
+
+  //目標達成プロセス生成中画面に遷移する関数
+  function goPage2() {
+    window.location.href = './Page2'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      {/* パスとコンポーネントの対応付けをする部分 */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Page1" element={<Page1 />} />
+        <Route path="/Page2" element={<Page2 />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
-export default App;
